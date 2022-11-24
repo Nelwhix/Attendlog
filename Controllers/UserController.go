b35a005044c147788643ace1e7f3a2205d5bff15
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2022-11-24 18:09:44 +0100

    working on dashboard

--- Controllers/UserController.go
+++ Controllers/UserController.go
@@ -1,10 +1,10 @@
 package Controllers
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-
 	"github.com/asaskevich/govalidator"
 	"github.com/gorilla/schema"
 )
@@ -20,7 +20,7 @@ type User struct {
 }
 
 func RenderLogin(w http.ResponseWriter, r *http.Request) {
-	parsedTemplate, _ := template.ParseFiles("views/login.html")
+	parsedTemplate, _ := template.ParseFiles("../views/login.html")
 	err := parsedTemplate.Execute(w, nil)
 
 	if err != nil {
@@ -29,27 +29,62 @@ func RenderLogin(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func Login(w http.ResponseWriter, r *http.Request) {
-	// map form to struct
+func RenderDashboard(w http.ResponseWriter, r *http.Request) {
+	parsedTemplate, _ := template.ParseFiles("../views/dashboard.html")
+	err := parsedTemplate.Execute(w, nil)
+
+	if err != nil {
+		log.Printf("Error occured while executing the template or writing its output : %v", err)
+		return
+	}
+}
+
+func readLoginForm(r *http.Request) *User {
 	r.ParseForm()
 	user := new(User)
 	decoder := schema.NewDecoder()
 	decodeErr := decoder.Decode(user, r.PostForm)
 
-	// server-side validation
+	if decodeErr != nil {
+		log.Printf("error mapping parsed form data to struct : %v", decodeErr)
+	}
+
+	return user
+}
+
+func validateUser(w http.ResponseWriter, r *http.Request, user *User) (bool, string) {
 	valid, validationErr := govalidator.ValidateStruct(user)
-	
+
 	if !valid {
 		userNameErr := govalidator.ErrorByField(validationErr, "Username")
 		passwordErr := govalidator.ErrorByField(validationErr, "Password")
 
 		if userNameErr != "" {
 			log.Printf("Username validation error : %v", userNameErr)
+			return valid, "Please fill in a valid Username"
 		}
 
 		if passwordErr != "" {
-			log.Printf("password validation error: %v", passwordErr)
+			log.Printf("Password validation error : %v", passwordErr)
+			return valid, "Please fill in a valid password"
 		}
 	}
 
+	return valid, "Validation error"
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	user := readLoginForm(r)
+	valid, validationErr := validateUser(w, r, user)
+
+	if !valid {
+		fmt.Fprint(w, validationErr)
+		return
+	}
+
+	if (user.Username == UserName && user.Password == Password) {
+		r
+	} else {
+		fmt.Fprintf(w, "Bad credentials")
+	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"os"
 	"log"
 	"net/http"
 	"github.com/gorilla/handlers"
@@ -14,27 +13,19 @@ const (
 	CONN_PORT = "8080"
 )
 
-func init() {
-	file, err := os.OpenFile("applogs.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	logger := log.New(file, "App Log", log.LstdFlags)
-}
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", Controllers.RenderAttendanceForm).Methods("GET")
 	router.HandleFunc("/admin", Controllers.RenderLogin).Methods("GET")
+	router.HandleFunc("/dashboard", Controllers.RenderDashboard).Methods("GET")
 	router.HandleFunc("/admin", Controllers.Login).Methods("POST")
 	router.HandleFunc("/", Controllers.SubmitAttendance).Methods("POST")
 	router.PathPrefix("/").Handler(http.StripPrefix("/resources", http.FileServer(http.Dir("resources/"))))
 
 	handlers.CompressHandler(router)
-	logger.Printf("Server starting on port %v\n", CONN_PORT)
+	log.Printf("Server starting on port %v\n", CONN_PORT)
 	err := http.ListenAndServe(CONN_HOST + ":" + CONN_PORT, router)
 
 	if err != nil {
